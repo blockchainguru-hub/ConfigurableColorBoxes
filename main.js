b5dc0d290ce18ad32729aeb1575f037ceaa57619
Praveen K. Sharma <8110596+blockchainguru-hub@users.noreply.github.com>
2024-08-08 15:41:07 +0530

    Basic scaffolding for box problem

---
+++ main.js
@@ -0,0 +1,9 @@
+const boxConfig = [
+  { color: "red", size: "33.33%" },
+  { color: "green", size: "33.33%" },
+  { color: "blue", size: "33.33%" },
+  { color: "yellow", size: "70%" },
+  { color: "orange", size: "30%" },
+  { color: "purple", size: "50%" },
+  { color: "brown", size: "50%" },
+];
